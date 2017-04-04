928ae8664ef35d78b709b1e5d718545b4972b63e
yoichi kobayashi <info@tplh.net>
2017-04-05 00:34:29 +0900

    [modify] rendering pug dynamic on browserSync.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,13 +1,40 @@
 const gulp = require('gulp');
 const yargs = require('yargs').argv;
 const browserSync = require('browser-sync');
+const path = require("path");
+const fs = require('fs');
+const url = require("url");
+const pug = require('pug');
 
 const conf = require('../conf').serve;
+const confPug = require('../conf').pug;
 
-gulp.task('serve', () => {
+const getPugTemplatePath = (baseDir, req)=>{
+  const requestPath = url.parse(req.url).pathname;
+  const suffix = path.parse(requestPath).ext ? '' : 'index.html';
+  return path.join(baseDir, "src/html", requestPath, suffix);
+}
+
+const pugMiddleWare = (req, res, next) => {
+  const requestPath = getPugTemplatePath(process.cwd(), req);
+  if (path.parse(requestPath).ext !== '.html') {
+    return next();
+  }
+  const pugPath = requestPath.replace('.html', '.pug');
+  console.log("[BS] Rendering pug file : "+ pugPath);
+  const content = pug.renderFile(pugPath, {
+    data: JSON.parse(fs.readFileSync(confPug.json)),
+    pretty: true,
+  });
+  res.end(new Buffer(content));
+}
+
+gulp.task("serve",()=> {
   if (yargs.build == true) {
+    conf.build.server.middleware = [pugMiddleWare];
     browserSync(conf.build);
   } else {
+    conf.dest.server.middleware = [pugMiddleWare];
     browserSync(conf.dest);
   }
 });
