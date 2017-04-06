7ea35290fa918d55bdcbfce38d06e44e130b3652
yoichi kobayashi <info@tplh.net>
2017-04-06 17:21:30 +0900

    [add] new function play, pause and stop for video player.

--- src/js/modules/buildVideoPlayer.js
+++ src/js/modules/buildVideoPlayer.js
@@ -1,14 +1,24 @@
 export default function(id, videoId) {
   return new Vue({
-    el: id,
+    el: `#${id}`,
     data: {
-      media: document.getElementById(videoId)
+      media: null,
+      time: 0,
+      duration: 0
     },
     mounted: function() {
-      this.media.play();
     },
     methods: {
-
+      play: function(event) {
+        this.media.play();
+      },
+      pause: function(event) {
+        this.media.pause();
+      },
+      stop: function(event) {
+        this.media.currentTime = 0;
+        this.media.pause();
+      },
     }
   });
 }
